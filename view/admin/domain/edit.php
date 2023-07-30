6717f98930cd61e8797d95baa93568633e9fb4a0
NaysKutzu <polargaiming@yahoo.com>
2023-07-30 20:10:40 +0000

    PUSH

--- view/admin/domain/edit.php
+++ view/admin/domain/edit.php
@@ -1,9 +1,5 @@
 <?php
 require('../class/session.php');
-function pass()
-{
-    return null;
-}
 $userdb = $conn->query("SELECT * FROM atoropics_users WHERE api_key = '" . mysqli_real_escape_string($conn, $_SESSION["api_key"]) . "'")->fetch_array();
 
 if ($userdb['admin'] == "false") {
