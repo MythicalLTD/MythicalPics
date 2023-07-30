03e53b05a619fcecc448cd7bb0919f363eee0271
NaysKutzu <polargaiming@yahoo.com>
2023-07-30 19:06:22 +0000

    PUSH

--- api/upload.php
+++ api/upload.php
@@ -1,6 +1,5 @@
 <?php
 header('Content-Type: application/json');
-
 if (isset($_POST['api_key'])) {
     $query = "SELECT * FROM atoropics_users WHERE api_key='" . $_POST['api_key'] . "'";
     $results = mysqli_query($conn, $query);
@@ -24,9 +23,14 @@ if (isset($_POST['api_key'])) {
                 if (!file_exists('../public/storage/json')) {
                     mkdir('json', 0777, true);
                 }
-                $checkCommand = "cd /var/www/MythicalPics && git pull";
-                ssh2_exec($connection, $checkCommand);
-                ssh2_disconnect($connection);
+                if ($_ENV['DISABLE_UPDATES'] == "true") { 
+                    $checkCommand = "cd /var/www/MythicalPics && git pull";
+                    ssh2_exec($connection, $checkCommand);
+                    ssh2_disconnect($connection);
+                }
+                else {
+
+                }
                 $imgname_c = time();
                 $new_name = $imgname_c . '.' . $ext;
                 $upload_folder = '../public/storage/uploads/';
@@ -36,7 +40,6 @@ if (isset($_POST['api_key'])) {
                     $imgurl = $settings['app_proto'] . $settings['app_url'] . "/storage/uploads/" . $new_name;
                     $date = date("Y-m-d H:i:s");
                     $filesize = filesize($upload_folder . $new_name);
-
                     if ($filesize >= 1073741824) {
                         $filesize = round($filesize / 1073741824, 2) . ' GB';
                     } elseif ($filesize >= 1048576) {
@@ -46,7 +49,6 @@ if (isset($_POST['api_key'])) {
                     } else {
                         $filesize = $filesize . ' bytes';
                     }
-
                     $data = array(
                         'url' => $imgurl,
                         'username' => $username,
@@ -57,13 +59,10 @@ if (isset($_POST['api_key'])) {
                         'date' => $date,
                         'filesize' => $filesize
                     );
-
                     $json = json_encode($data, JSON_PRETTY_PRINT);
                     file_put_contents("../public/storage/json/" . $imgname_c . '.json', $json);
-
                     $query = "SELECT domain FROM atoropics_domains WHERE ownerkey = '" . $userdb['api_key'] . "'";
                     $result = $conn->query($query);
-
                     if ($result) {
                         $row = $result->fetch_assoc();
                         $domain = $row['domain'];
@@ -71,7 +70,6 @@ if (isset($_POST['api_key'])) {
                     } else {
                         echo $settings['app_proto'] . $settings['app_url'] . "/i?i=" . $imgname_c;
                     }
-
                     $apikey = $_POST['api_key'];
                     $conn->query("INSERT INTO atoropics_imgs (name, owner_key, size, storage_folder) VALUES ('$imgname_c', '$apikey', '$filesize', '$imgurl')");
 
@@ -80,17 +78,21 @@ if (isset($_POST['api_key'])) {
                     unlink('lockfile-'.$imgname_c.'.lock');
                 } else {
                     echo json_encode(array('status' => 'error', 'message' => 'Failed to upload file'));
+                    $conn->close();
+                    die();
                 }
             } else {
-                echo json_encode(array('status' => 'error', 'message' => 'Invalid file type'));
+                $conn->close();
+                die(json_encode(array('status' => 'error', 'message' => 'Invalid file type')));
             }
         } else {
-            echo json_encode(array('status' => 'error', 'message' => 'No file uploaded'));
+            $conn->close();
+            die(json_encode(array('status' => 'error', 'message' => 'No file uploaded')));
         }
     } else {
-        echo json_encode(array('status' => 'error', 'message' => 'Invalid API key'));
+        die(json_encode(array('status' => 'error', 'message' => 'Invalid API key')));
     }
 } else {
-    echo json_encode(array('status' => 'error', 'message' => 'No API key'));
+    die(json_encode(array('status' => 'error', 'message' => 'No API key')));
 }
 ?>
